646033e2ac438748b3a201e98f44ac4a5af5013f
ianibo <ianibbo@googlemail.com>
2013-03-13 17:00:28 +0000

    Completely removed references to bootstrap plugin

--- app/grails-app/conf/ScaffoldingResources.groovy
+++ app/grails-app/conf/ScaffoldingResources.groovy
@@ -2,9 +2,9 @@ import org.codehaus.groovy.grails.commons.ApplicationHolder
 
 modules = {
   scaffolding {
-    dependsOn 'bootstrap'
+    // dependsOn 'bootstrap'
    // resource url: 'css/scaffolding.css'
-    resource url: 'css/bootstrap.css'
+    // resource url: 'css/bootstrap.css'
     resource url: 'css/style.css'
     resource url: "css/${ApplicationHolder.application.config.defaultCssSkin?:'live.css'}"
   }
